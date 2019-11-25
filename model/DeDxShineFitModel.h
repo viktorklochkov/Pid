7e0cfde8637f644ced2ae0f90ad602ca2423be78
Evgeny Kashirin <kashirin.e@list.ru>
2019-11-26 00:11:44 +0300

    Add AsymmetricGaussPDF for the Shine dEdx fitting model

---
+++ model/DeDxShineFitModel.h
@@ -0,0 +1,17 @@
+//
+// Created by eugene on 25/11/2019.
+//
+
+#ifndef PID_MODEL_DEDXSHINEFITMODEL_H_
+#define PID_MODEL_DEDXSHINEFITMODEL_H_
+
+#include <TMath.h>
+#include <RooAbsPdf.h>
+#include <RooRealProxy.h>
+#include <RooRealVar.h>
+
+class DeDxShineFitModel {
+
+};
+
+#endif //PID_MODEL_DEDXSHINEFITMODEL_H_
