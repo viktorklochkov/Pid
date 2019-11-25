780127f65e68166c3248fd19a2952e6e0b2f6ed6
Evgeny Kashirin <kashirin.e@list.ru>
2019-11-25 22:09:40 +0300

    add Bethe-Bloch implementations from Shine

---
+++ bb/shine.h
@@ -0,0 +1,35 @@
+//
+// Created by eugene on 25/11/2019.
+//
+
+#ifndef PID_BB_SHINE_H_
+#define PID_BB_SHINE_H_
+
+
+/**
+ * @brief a Bethe-Block implementation adjusted by Antoni Marcinek
+ * used in calibration and it is preferred to be used in the analysis
+ * @param betaGamma
+ * @return dEdx (MIP)
+ */
+double
+BetheBlochAntoniMod(double betaGamma);
+
+/**
+ * @brief ??
+ * @param betaGamma
+ * @return
+ */
+double
+BetheBlochAntoni(double betaGamma);
+
+/**
+ * @brief a Bethe-Bloch implementation used in ShineOffline
+ * @param betaGamma
+ * @return
+ */
+double
+TPC_GetBetheBlochValue(double betaGamma);
+
+
+#endif //PID_BB_SHINE_H_
