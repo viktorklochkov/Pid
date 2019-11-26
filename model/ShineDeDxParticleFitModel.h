33f9a7e8a942e11d63367bc1970eaaa534402bc6
Evgeny Kashirin <kashirin.e@list.ru>
2019-11-26 22:10:13 +0300

    Add Base and Shine fit model

---
+++ model/ShineDeDxParticleFitModel.h
@@ -0,0 +1,31 @@
+//
+// Created by eugene on 26/11/2019.
+//
+
+#ifndef PID_SHINEDEDXPARTICLEFITMODEL_H
+#define PID_SHINEDEDXPARTICLEFITMODEL_H
+
+
+#include "BaseParticleFitModel.h"
+
+class ShineDeDxParticleFitModel : public BaseParticleFitModel {
+
+public:
+    ShineDeDxParticleFitModel(int pdgId);
+
+    void initModel() override;
+
+    RooAbsPdf *getFitModel() override;
+
+    std::vector<RooAbsReal *> getFitParams() override;
+
+private:
+    RooAbsPdf *pdf_;
+    RooRealVar *bb;
+    RooRealVar *sigma;
+    RooRealVar *d;
+
+};
+
+
+#endif //PID_SHINEDEDXPARTICLEFITMODEL_H
