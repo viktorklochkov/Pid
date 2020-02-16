fd4dfbefabd1126b4dd022df992e56f76ea3367c
Viktor <klochkov44@gmail.com>
2020-02-16 16:03:25 +0100

    RootFit

--- macro/RooFitTest.C
+++ macro/RooFitTest.C
@@ -15,38 +15,58 @@ void RooFitTest()
   std::unique_ptr <TH2D> hproton {(TH2D*) (fIn->Get("hTof_p_M2"))};
   std::unique_ptr <TH2D> hpos {(TH2D*) (fIn->Get("hTofM2Pos"))};
 
-  RooRealVar qp("qp","qp",1,5) ;
-  RooRealVar m2("m2","m2",-0.2,0.2) ;
+  RooRealVar qp("qp","qp",1.,6) ;
+  RooRealVar m2("m2","m2",0.5,1.2) ;
 
-  RooRealVar mean_a("mean_a","mean_a", 0.02, 0.0, 0.1);
+  RooRealVar mean_a("mean_a","mean_a", 0.02, 0.0, 0.05);
   RooRealVar mean_b("mean_b","mean_b", -0.01, -0.05, 0.);
 
-  RooRealVar sigma_a("sigma_a","sigma_a",0.02, 0., 0.1);
-  RooRealVar sigma_b("sigma_b","sigma_b", 0., -0.01, 0.01);
-  RooRealVar sigma_c("sigma_c","sigma_c", 0.02, 0., 0.2);
+  RooRealVar mean_kaon_a("mean_kaon_a","mean_kaon_a", 0.25, 0.23, 0.27);
+  RooRealVar mean_kaon_b("mean_kaon_b","mean_kaon_b", -0.01, -0.05, 0.);
+
+  RooRealVar mean_p_a("mean_p_a","mean_p_a", 0.9, 0.85, 0.95);
+  RooRealVar mean_p_b("mean_p_b","mean_p_b", -0.01, -0.05, 0.);
+
+  RooRealVar sigma_a("sigma_a","sigma_a",0.02, 0., 0.05);
+  RooRealVar sigma_b("sigma_b","sigma_b", -0.01, -0.1, 0.1);
+  RooRealVar sigma_c("sigma_c","sigma_c", 0.004, 0., 0.01);
 
   RooRealVar k("k","k", 1.5, 1., 2);
+  RooRealVar k_kaon("k_kaon","k_kaon", 1.0, 0.7, 1.2);
+  RooRealVar k_p("k_p","k_p", 0.5, 0.3, 0.7);
 
   RooFormulaVar mean("mean","mean_a+mean_b*qp", RooArgList(mean_a, mean_b, qp)) ;
-  RooFormulaVar sigma("sigma","sigma_a+sigma_b*qp+sigma_c*qp**2", RooArgList(sigma_a, sigma_b, sigma_c, qp)) ;
-  RooFormulaVar A("A","exp(-k*qp)", RooArgList(k,qp)) ;
+  RooFormulaVar sigma("sigma","sigma_a + sigma_c*qp**2", RooArgList(sigma_a, sigma_c, qp)) ;
+//  RooFormulaVar sigma("sigma","sigma_a + sigma_b*qp + sigma_c*qp**2", RooArgList(sigma_a, sigma_b, sigma_c, qp)) ;
+  RooFormulaVar A("A","exp(-k*qp)", RooArgList(k, qp)) ;
 
-  RooGenericPdf fit_func("fit_func","fit_func","A*exp(-(m2-mean)**2/2/sigma**2)",RooArgSet(A,mean,sigma, qp, m2)) ;
+  RooFormulaVar mean_kaon("mean_kaon","mean_kaon_a+mean_kaon_b*qp", RooArgList(mean_kaon_a, mean_kaon_b, qp)) ;
+  RooFormulaVar A_kaon("A_kaon","exp(-k_kaon*qp)", RooArgList(k_kaon, qp)) ;
 
-  RooPlot* xframe = m2.frame(Title("Gaussian p.d.f.")) ;
+  RooFormulaVar mean_p("mean_p","mean_p_a+mean_p_b*qp", RooArgList(mean_p_a, mean_p_b, qp)) ;
+  RooFormulaVar A_p("A_p","exp(-k_p*qp)", RooArgList(k_p, qp)) ;
 
-  RooDataHist dh("dh","dh", RooArgList(qp,m2), Import(*hpion));
+  RooGenericPdf fit_kaon("fit_kaon","fit_kaon","A_kaon*exp(-(m2-mean_kaon)**2/2/sigma**2)",RooArgSet(A_kaon, mean_kaon, sigma, qp, m2)) ;
+  RooGenericPdf fit_pion("fit_pion","fit_pion","A*exp(-(m2-mean)**2/2/sigma**2)",RooArgSet(A,mean,sigma, qp, m2)) ;
+  RooGenericPdf fit_p("fit_p","fit_p","A_p*exp(-(m2-mean_p)**2/2/sigma**2)",RooArgSet(A_p,mean_p,sigma, qp, m2)) ;
 
-  dh.plotOn(xframe);
-  fit_func.fitTo(dh, "e");
-  fit_func.plotOn(xframe);
+  RooRealVar kaon_frac("kaon_frac","kaon_frac",0.05, 0.01, 0.1) ;
+  RooRealVar pion_frac("pion_frac","pion_frac",0.4, 0.2, 0.7) ;
 
-  TH2* data = qp.createHistogram("x vs y pdf",m2);
+  RooAddPdf sum("sum","fit_pion+fit_kaon+fit_p",RooArgList(fit_kaon,fit_pion, fit_p), RooArgList(pion_frac, kaon_frac));
+
+  RooDataHist dh("dh","dh", RooArgList(qp,m2), Import(*hpos));
+
+  fit_p.fitTo(dh);
+
+  TH2* data = qp.createHistogram("x vs y pdf", m2);
   dh.fillHistogram(data, RooArgList(qp, m2)) ;
 
   TH2* fit = qp.createHistogram("x vs y data",m2);
-  fit_func.fillHistogram(fit, RooArgList(qp, m2)) ;
+  fit_p.fillHistogram(fit, RooArgList(qp, m2)) ;
 
+  const float scale = fit->GetMaximum() / data->GetMaximum();
+  data->Scale(scale);
 
   TCanvas* c = new TCanvas("rf102_dataimport","rf102_dataimport",800,800);
   c->Divide(2,2);
@@ -62,9 +82,23 @@ void RooFitTest()
   temp->Divide(data);
   temp->Draw("colz");
 
-  gPad->Update();
-  c->SaveAs("test.root");
+  c->cd(4);
+  RooPlot* xframe = m2.frame(Title("Gaussian p.d.f.")) ;
+//  dh.plotOn(xframe, Cut("abs(qp-2)<0.01"));
+  qp=2;
+  sum.plotOn(xframe, Slice(qp));
+  xframe->Draw();
 
+  gPad->Update();
+
+  TFile* f = TFile::Open("test.root", "recreate");
+
+  temp->Write("ratio");
+  fit->Write("fit");
+  data->Write("data");
+  c->Write("canvas");
+
+  f->Close();
 }
 
 TH2D* TestHisto()
