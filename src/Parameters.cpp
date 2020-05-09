1c483e37b9d05e2daf7cae972ba7565cfbdf0764
Oleg Golosov <oleg.golosov@gmail.com>
2020-05-09 18:56:04 +0300

    fix compatibility with newer ROOT versions

--- src/Parameters.cpp
+++ src/Parameters.cpp
@@ -39,39 +39,36 @@ void Parameters::Parametrize( std::vector <ParticleFit> &particles)
                 
                 y.push_back(par);
                 dy.push_back(par_err);
-
             }
             
-	    TGraphErrors par( params_.size(), &(x_[0]), &(y[0]), 0, &(dy[0]) );
-	    TF1 &fit = particles.at(ipart).GetParametrizationFunction(ivar);
-	    par.SetName( fit.GetName() );
-	    par.SetTitle( Form("%s;p (GeV/#it{c});%s", fit.GetName(), fit.GetName()) );
+            TF1 &fit = particles.at(ipart).GetParametrizationFunction(ivar);
+            TGraphErrors *par = new TGraphErrors ( params_.size(), &(x_[0]), &(y[0]), 0, &(dy[0]) );
+            par->SetName( fit.GetName() );
+            par->SetTitle( Form("%s;p (GeV/#it{c});%s", fit.GetName(), fit.GetName()) );
+
             if (strcmp(fit.GetTitle(), "0") == 0)
             {
-	      std::function <double (const double*, const double*)> fitFunc=[=](const double *x, const double *) { return par.Eval(x[0]);};
-	      TF1 *false_fit = new TF1(fit.GetName(), fitFunc, fit.GetXmin(), fit.GetXmax(),0); 
-	      false_fit->SetNpx(par.GetN()*5);
-	      false_fit->SetTitle("0");
-	      particles.at(ipart).SetParametrizationFunction(ivar, *false_fit);
-	      par.GetListOfFunctions()->Add(false_fit);
-	      par.Draw();
-	    }
-	    else
-	    {
-	      if (particles.size() > 1)
-	      {
-	          float pmin, pmax;
-                  particles.at(ipart).GetRange(pmin, pmax);
-                  par.Fit( &fit, "Q,M", "", pmin, pmax); 
-	      }
-	      else 
-                  par.Fit( &fit, "Q,M,R"); 
-	    }
-            par.Write();
-            graphs.push_back( par );
-        }
-        
-        
+                std::function <double (const double*, const double*)> fitFunc=[=](const double *x, const double *) { return par->Eval(x[0]);};
+                TF1 false_fit (fit.GetName(), fitFunc, fit.GetXmin(), fit.GetXmax(), 0); 
+                false_fit.SetNpx(par->GetN()*5);
+                false_fit.SetTitle("0");
+                particles.at(ipart).SetParametrizationFunction(ivar, false_fit);
+                par->GetListOfFunctions()->Add(false_fit.Clone());
+            }
+            else
+            {
+                if (particles.size() > 1)
+                {
+                    float pmin, pmax;
+                        particles.at(ipart).GetRange(pmin, pmax);
+                        par->Fit( &fit, "Q,M", "", pmin, pmax); 
+                }
+                else 
+                    par->Fit( &fit, "Q,M,R"); 
+            }
+            par->Write();
+            graphs.push_back( *par );
+        }        
     }
 }
 
